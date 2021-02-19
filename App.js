685ec6d2f61352dbeede63049378684e6219676d
Carlos H <carlos.e.herrera.j@gmail.com>
2021-02-19 17:57:17 -0400

    Sync - 19/02/2021 - 05:57 pm
    Sync - 19/02/2021 - 05:57 pm

--- App.js
+++ App.js
@@ -1,21 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+/*import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';*/
+
+import Navigation from './navigations/Navigation';
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <Navigation/>
+
+    /*<View style={styles.container}>
       <Text>Hello world...!!!</Text>
       <StatusBar style="auto" />
-    </View>
+    </View>*/
   );
 }
 
-const styles = StyleSheet.create({
+/*const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
-});
+});*/
