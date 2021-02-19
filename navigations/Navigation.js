59811bae68bc194923749f650286379784a11dca
Carlos H <carlos.e.herrera.j@gmail.com>
2021-02-19 18:09:03 -0400

    Sync - 19/02/2021 - 06:08 pm
    Sync - 19/02/2021 - 06:08 pm

--- navigations/Navigation.js
+++ navigations/Navigation.js
@@ -3,6 +3,10 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
 import Restaurants from "../screens/Restaurants";
+import Favorites from "../screens/Favorites";
+import TopRestaurants from "../screens/TopRestaurants";
+import Search from "../screens/Serach";
+import Account from "../screens/Account";
 
 const Tab = createBottomTabNavigator();
 
@@ -10,8 +14,27 @@ export default function Navigation() {
   return (
     <NavigationContainer>
       <Tab.Navigator>
+
           <Tab.Screen name="restaurants"
-            component={Restaurants}/>
+            component={Restaurants} 
+            options={{title: "Restaurants"}}/>
+
+          <Tab.Screen name="favorites"
+            component={Favorites}
+            options={{title: "Favorites"}}/>
+
+          <Tab.Screen name="Top restaurants"
+            component={TopRestaurants} 
+            options={{title: "Top Restaurants"}}/>
+
+          <Tab.Screen name="Search"
+            component={Search}
+            options={{title: "Search"}}/>
+
+          <Tab.Screen name="Account"
+            component={Account}
+            options={{title: "Account"}}/>
+
       </Tab.Navigator>
     </NavigationContainer>
   );
