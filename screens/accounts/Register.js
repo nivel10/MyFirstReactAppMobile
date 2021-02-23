ca6af87bf15e607b1926abe878f8a3fd25e408f6
Carlos H <carlos.e.herrera.j@gmail.com>
2021-02-23 17:29:20 -0400

    Sync - 23/02/2021 - 05:29 pm
    Sync - 23/02/2021 - 05:29 pm

--- screens/accounts/Register.js
+++ screens/accounts/Register.js
@@ -1,14 +1,34 @@
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, Image} from 'react-native'
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
 
 import RegisterForm from '../../components/acount/RegisterForm'
 
 export default function Register() {
     return (
-        <View>
+        <KeyboardAwareScrollView
+            style={styles.container}
+        >
+              <Image
+                source={require("../../assets/fhb.png")}
+                resizeMode="contain"
+                style={styles.image}
+            />
             <RegisterForm/>
-        </View>
+        </KeyboardAwareScrollView>
     )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    image: {
+        height: 150,
+        width: "100%",
+        marginBottom: 30,
+        marginTop: 10,
+    },
+
+    container:{
+        flex: 1,
+        marginTop: 30,
+    },
+})
