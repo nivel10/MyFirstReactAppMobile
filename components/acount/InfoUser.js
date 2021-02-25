6e82deedca0c740da7067c015e06fc8a3ca24271
Carlos H <carlos.e.herrera.j@gmail.com>
2021-02-25 17:37:07 -0400

    Sync - 25/04/2021 - 05:37 pm
    Sync - 25/04/2021 - 05:37 pm

--- components/acount/InfoUser.js
+++ components/acount/InfoUser.js
@@ -1,12 +1,57 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { Avatar } from "react-native-elements";
 
-export default function InfoUser({ user, }) {
-    return (
-        <View>
-            <Text>Info user (Component)</Text>
-        </View>
-    )
+export default function InfoUser({ user }) {
+  console.log(user);
+
+  return <View 
+            style={styles.viewContainer}
+        >
+            <Avatar
+                rounded={true}
+                size="large"
+                source={
+                    user.photoURL !== null ? 
+                        {uri: user.photoURL }: 
+                            require("../../assets/avatar-default.png")
+                }
+            />
+            <View 
+                style={styles.viewInfoUser}
+            >
+                <Text
+                    style={styles.textDisplayName}
+                >
+                    {
+                        user.displayName !== null ?
+                            user.displayName :
+                                "No name yet"
+                    }
+
+                </Text>
+                <Text>{user.email}</Text>
+            </View>
+
+        </View>;
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  viewContainer: {
+      alignItems: "center",
+      justifyContent:"center",
+      flexDirection:"row",
+      backgroundColor: "#f9f9f9",
+      paddingVertical: 30,
+  },
+
+  viewInfoUser: {
+      marginLeft: 20,
+  },
+
+  textDisplayName: {
+      fontWeight:"bold",
+      paddingBottom: 5,
+  }
+
+});
