27c0b498c80025a1f9075d24cc780e65cf3769e9
Carlos H <carlos.e.herrera.j@gmail.com>
2021-03-02 18:06:09 -0400

    Sync - 02/03/2021 - 06:06 pm
    Sync - 02/03/2021 - 06:06 pm

--- screens/restaurants/Restaurants.js
+++ screens/restaurants/Restaurants.js
@@ -1,12 +1,58 @@
-import React from 'react'
+import React, {useState, useEffect, } from 'react'
 import { StyleSheet, Text, View } from 'react-native'
+import { Icon } from 'react-native-elements'
+import firebase from 'firebase/app';
+
+import Loading from '../../components/Loading';
 
 export default function Restaurants() {
+
+    const [user, setUser] = useState(null);
+
+    useEffect(() => {
+        firebase.auth().onAuthStateChanged((userInfo) => {
+            userInfo ? setUser(true) : setUser(false);
+        });
+    }, []);
+
+    /*if(user === null){
+        return <Loading
+            isVisible={true} 
+            text="Loading, please wait..."
+        />
+    }*/
+
     return (
-        <View>
-            <Text>Restaurants</Text>
+        <View
+            style={styles.viewBody}
+        >
+            <Text>Restaurants...</Text>
+            {
+                user && (
+                    <Icon
+                        type="material-community"
+                        name="plus"
+                        color="#f2936c"
+                        reverse={true}
+                        containerStyle={styles.btnIconContainer}
+                    />
+                )
+            }
         </View>
     )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    viewBody: {
+        flex: 1,
+    },
+
+    btnIconContainer: {
+        position: "absolute",
+        bottom: 10,
+        right: 10,
+        shadowColor: "gray",
+        shadowOffset: {width: 2, height: 2, },
+        shadowOpacity: 0.5,
+    }
+})
